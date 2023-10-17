b13278f46d25051c939ad40eafbe8e73d63fa7e9
Daniela Ivanova <134614414+DaniKIvanova@users.noreply.github.com>
2023-10-17 22:40:11 +0300

    updates

---
+++ 04_Multidimensional_Lists/exercise_1/4_maximal_sum.py
@@ -0,0 +1,23 @@
+def create_matrix(rows):
+    result = []
+    for _ in range(rows):
+        result.append(list(int(el) for el in input().split()))
+    return result
+
+
+rows, columns = [int(el) for el in input().split()]
+matrix = create_matrix(rows)
+
+max_sum_square = -9999999999999
+
+for r in range(rows - 2):
+    for c in range(columns - 2):
+        sum_square = sum(matrix[r][c:c + 3]) + sum(matrix[r+1][c:c + 3]) + sum(matrix[r+2][c:c + 3])
+        if sum_square > max_sum_square:
+            max_sum_square = sum_square
+            max_square = [matrix[r][c:c + 3], matrix[r+1][c:c + 3], matrix[r+2][c:c + 3]]
+
+print(f"Sum = {max_sum_square}")
+
+for el in max_square:
+    print(*el, sep=" ")
