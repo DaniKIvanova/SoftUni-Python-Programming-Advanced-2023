a38cab9cd7d83f65308cc00454522297bbc4bedb
Daniela Ivanova <134614414+DaniKIvanova@users.noreply.github.com>
2023-10-21 23:52:54 +0300

    Create 8_recursion_palindrome.py

---
+++ 05_Functions_Advanced/exercise/8_recursion_palindrome.py
@@ -0,0 +1,17 @@
+def palindrome(word, index=0, reversed_word=""):
+
+    if index == len(word):
+
+        if not reversed_word == word:
+            return f"{word} is not a palindrome"
+
+        return f"{word} is a palindrome"
+
+    else:
+        reversed_word += word[-(index + 1)]
+        return palindrome(word, index + 1, reversed_word)
+
+
+print(palindrome("abcba"))
+print(palindrome("peter"))
+
